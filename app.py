67c9947bd4639877bf24a5bec0cf781ebbd006d2
0722976479 <ndou4949@gmail.com>
2026-09-13 22:14:02 +0200

    Set up Flask app with home route

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "<h1>🇿🇦 South Africa Helper - LIVE!</h1><p>Site is working!</p>"
+
+if __name__ == '__main__':
+    app.run()
